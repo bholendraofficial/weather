4c1f973da485aa886e416fa04f3a4c43937b5dea
Bholendra Singh <bholendraofficial@gmail.com>
2025-01-20 21:33:25 +0530

    gradient added

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -4,17 +4,27 @@ class Weather {
   final double minTemperature;
   final double maxTemperature;
   final String description;
+  final String main;
   final String icon;
   final int humidity;
   final double windSpeed;
+  final int windDeg;
+  final double windGust;
   final String country;
   final String cityName;
   final double lat;
   final double lon;
   final double visibility;
   final int pressure;
+  final int seaLevel;
+  final int grndLevel;
+  final int clouds;
   final int sunrise;
   final int sunset;
+  final int dt;
+  final int timezone;
+  final int id;
+  final int cod;
 
   Weather({
     required this.temperature,
@@ -22,17 +32,27 @@ class Weather {
     required this.minTemperature,
     required this.maxTemperature,
     required this.description,
+    required this.main,
     required this.icon,
     required this.humidity,
     required this.windSpeed,
+    required this.windDeg,
+    required this.windGust,
     required this.country,
     required this.cityName,
     required this.lat,
     required this.lon,
     required this.visibility,
     required this.pressure,
+    required this.seaLevel,
+    required this.grndLevel,
+    required this.clouds,
     required this.sunrise,
     required this.sunset,
+    required this.dt,
+    required this.timezone,
+    required this.id,
+    required this.cod,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
@@ -42,17 +62,27 @@ class Weather {
       minTemperature: json['main']['temp_min'].toDouble(),
       maxTemperature: json['main']['temp_max'].toDouble(),
       description: json['weather'][0]['description'],
-      icon: json['weather'][0]['icon'],
+      main: json['weather'][0]['main'],
+      icon: "http://openweathermap.org/img/w/${json['weather'][0]['icon']}.png",
       humidity: json['main']['humidity'],
       windSpeed: json['wind']['speed'].toDouble(),
+      windDeg: json['wind']['deg'],
+      windGust: json['wind']['gust'].toDouble(),
       country: json['sys']['country'],
       cityName: json['name'],
       lat: json['coord']['lat'].toDouble(),
       lon: json['coord']['lon'].toDouble(),
       visibility: json['visibility'].toDouble(),
       pressure: json['main']['pressure'],
+      seaLevel: json['main']['sea_level'],
+      grndLevel: json['main']['grnd_level'],
+      clouds: json['clouds']['all'],
       sunrise: json['sys']['sunrise'],
       sunset: json['sys']['sunset'],
+      dt: json['dt'],
+      timezone: json['timezone'],
+      id: json['id'],
+      cod: json['cod'],
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,49 +12,115 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather App'),
-      ),
-      body: Consumer2<LocationProvider, WeatherProvider>(
-        builder: (context, locationProvider, weatherProvider, child) {
+      body: Consumer<WeatherProvider>(
+        builder: (context, weatherProvider, child) {
           if (weatherProvider.isLoading) {
             return const Center(child: CircularProgressIndicator());
           }
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                weatherProvider.weather != null
-                    ? Column(
-                        children: <Widget>[
-                          Text(
-                            'Temperature: ${weatherProvider.weather!.temperature}°C',
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            'Description: ${weatherProvider.weather!.description}',
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                          Image.network(
-                            'http://openweathermap.org/img/w/${weatherProvider.weather!.icon}.png',
-                          ),
-                          const SizedBox(height: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              weatherProvider.setLoading(false);
-                              // Optionally reset the weather or allow the user to fetch new data
-                            },
-                            child: const Text('Clear Weather Data'),
-                          ),
-                        ],
-                      )
-                    : Container(),
-              ],
+          if (weatherProvider.weather == null) {
+            return const Center(child: Text("Please try again"));
+          }
+
+          return Container(
+            decoration: getBackgroundGradient(weatherProvider.weather!.main), // Apply gradient background
+            child: Center(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      weatherProvider.weather != null
+                          ? Card(
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              color: Colors.blue.shade50,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '${weatherProvider.weather!.temperature}°C',
+                                          style: const TextStyle(
+                                            fontSize: 40,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.blue,
+                                          ),
+                                        ),
+                                        const SizedBox(width: 10),
+                                        Image.network(
+                                          weatherProvider.weather!.icon,
+                                          width: 50,
+                                          height: 50,
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                      weatherProvider.weather!.description,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontStyle: FontStyle.italic,
+                                        color: Colors.blueGrey,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Humidity: ${weatherProvider.weather!.humidity}%',
+                                              style:
+                                                  const TextStyle(fontSize: 18),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Text(
+                                              'Wind Speed: ${weatherProvider.weather!.windSpeed} m/s',
+                                              style:
+                                                  const TextStyle(fontSize: 18),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(width: 30),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              'Pressure: ${weatherProvider.weather!.pressure} hPa',
+                                              style:
+                                                  const TextStyle(fontSize: 18),
+                                            ),
+                                            const SizedBox(height: 10),
+                                            Text(
+                                              'Feels Like: ${weatherProvider.weather!.feelsLike}°C',
+                                              style:
+                                                  const TextStyle(fontSize: 18),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+              ),
             ),
           );
         },
@@ -74,9 +140,9 @@ class _HomeScreenState extends State<HomeScreen> {
   // Method to fetch the location and weather data
   void fetchLocation() {
     final weatherProvider =
-    Provider.of<WeatherProvider>(context, listen: false);
+        Provider.of<WeatherProvider>(context, listen: false);
     final locationProvider =
-    Provider.of<LocationProvider>(context, listen: false);
+        Provider.of<LocationProvider>(context, listen: false);
 
     weatherProvider.setLoading(true);
     locationProvider.getCurrentLocation().then((locationData) {
@@ -92,5 +158,58 @@ class _HomeScreenState extends State<HomeScreen> {
       weatherProvider.setLoading(false);
       print('An error occurred: $error');
     });
+  }
+
+  BoxDecoration getBackgroundGradient(String main) {
+    switch (main.toLowerCase()) {
+      case 'clear':
+        return const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.lightBlueAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Clear sky gradient
+      case 'clouds':
+        return BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.grey.shade300, Colors.grey.shade500],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Cloudy weather gradient
+      case 'rain':
+        return const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueGrey, Colors.indigo],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Rainy weather gradient
+      case 'storm':
+        return BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, Colors.grey.shade800],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Stormy weather gradient
+      case 'snow':
+        return BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.blue.shade100],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Snowy weather gradient
+      default:
+        return const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.lightBlue, Colors.blueAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ); // Default background gradient
+    }
   }
 }
